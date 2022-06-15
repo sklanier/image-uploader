010ea50c93af8f51daf74401c237113fdf569699
sklanier <stevekentlanier@gmail.com>
2022-06-15 11:20:05 -0500

    👍 IMPROVE: added gutters and fixed box dimensions

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="center-box">
-        
+
+      <div className="main-container">
+        <div className="center-box">
+  
+        </div>
       </div>
     </div>
   );
