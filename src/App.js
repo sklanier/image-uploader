864441dfce93687ff7b8164dcb9c5aa47d728770
sklanier <stevekentlanier@gmail.com>
2022-06-16 20:41:44 -0500

    👍 IMPROVE: taking a bit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
 
       <div className="main-container">
         <div className="center-box">
-  
+          <h1 className="title">
+            Upload Your Image.
+          </h1>
+          <h1 className="title">
+            Now. 
+          </h1>
         </div>
       </div>
     </div>
